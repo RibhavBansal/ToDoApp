37f7e34f64b5d1b3a45ceb53f024be7b044dd55e
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2023-09-30 16:55:14 +0530

    TodoDatabase

---
+++ app/src/main/java/com/example/todoapp/TodoDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Items::class,Groups::class], version = 1)
+abstract  class TodoDatabase : RoomDatabase()
+{
+    abstract  fun todoDao(): TodoDao
+    companion object{
+        private var instance: TodoDatabase? = null
+        fun getDatabase(context: Context): TodoDatabase {
+            if (instance == null)
+            {
+                synchronized(TodoDatabase::class)
+                {
+                    instance = Room.databaseBuilder(context.applicationContext,
+                                    TodoDatabase::class.java,
+                                    "temporary_name").build()
+                }
+            }
+            return instance!!
+        }
+    }
+}
