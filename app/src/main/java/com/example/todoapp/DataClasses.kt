ea5babb1097a2df4a9c04dc262cbd466495b7521
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2023-09-30 12:52:10 +0530

    UpdateDataClass

--- app/src/main/java/com/example/todoapp/DataClasses.kt
+++ app/src/main/java/com/example/todoapp/DataClasses.kt
@@ -1,7 +1,21 @@
 package com.example.todoapp
 
-data class Item(val name: String,
-                var completed: Boolean)
+import androidx.constraintlayout.widget.Group
+import androidx.room.*
 
-data class Group(val name: String,
-                var items: MutableList<Item>)
+@Entity
+data class Items(@ColumnInfo(name = "item_name") val name: String,
+                 @ColumnInfo(name = "group_name") val groupName: String,
+                var completed: Boolean)
+{
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
+
+data class Groups(@ColumnInfo(name = "group_name")val name: String)
+{
+    @PrimaryKey(autoGenerate = true)var id = 0
+}
+
+data class GroupWithItems(@Embedded val group: Groups,
+                          @Relation(parentColumn = "group_name",
+                          entityColumn = "group_name") val items: MutableList<Items>)
